81629cb7acac3841037d0196d2faf0222a41d1ce
hamidvisen <abdulhamid.31102@gmail.com>
2022-08-24 10:31:23 +0530

    HEllo

--- Addition1.java
+++ Addition1.java
@@ -3,7 +3,8 @@ public class Addition1 {
     public static void main(String[] args) {
         int a= 10 ; 
         int b= 23;
-        int sum = a+b;
+        int c= 54;
+        int sum = a+b + c;
         System.out.println("Sum is equal to"+" "+sum);
     }
     
