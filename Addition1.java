a57a3b21b9cc7d886d5b1f26504893c6114acea9
hamidvisen <95701708+hamidvisen@users.noreply.github.com>
2022-08-24 10:13:37 +0530

    Update Addition1.java

--- Addition1.java
+++ Addition1.java
@@ -1,6 +1,11 @@
 public class Addition1 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int sum1 = num1 + num2;
+        System.out.println(sum1);
         int a= 10 ; 
         int b= 23;
         int sum = a+b;
