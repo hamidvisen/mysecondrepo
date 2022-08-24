3fb72d4cc16b9ce2857afa36f62263085b559d4e
hamidvisen <95701708+hamidvisen@users.noreply.github.com>
2022-08-24 11:04:07 +0530

    Update Addition1.java

--- Addition1.java
+++ Addition1.java
@@ -3,8 +3,9 @@ public class Addition1 {
     public static void main(String[] args) {
         int a= 10 ; 
         int b= 23;
-    
-        int sum = a+b ;
+    int c =3;
+        
+        int sum = a+b +c ;
         System.out.println("Sum is equal to"+" "+sum);
     }
     
